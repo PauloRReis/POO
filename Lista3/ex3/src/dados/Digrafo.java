9da554c0023b03c07c34e2b75caf3b744f29dbd3
Paulo Ricardo dos Reis <paulo230301@hotmail.com>
2022-05-23 12:59:26 -0300

    lista3

---
+++ Lista3/ex3/src/dados/Digrafo.java
@@ -0,0 +1,21 @@
+package dados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Digrafo {
+    
+    private List<List<Integer>> matrizAdjacencia1;
+    private Map<Integer, List<Integer>> matrizAdjacencia2;
+
+    public void adicionarVertice(){
+
+        List<Integer> adiciona = new ArrayList<>();
+
+        matrizAdjacencia1.add(adiciona);
+
+    }
+
+
+}
